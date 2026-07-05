a886d69ef763a668bb8f06b6549cea9b7a92d270
yongzheng2024 <yongzheng2024@outlook.com>
2026-07-06 00:04:47 +0800

    update test file (generate smt encoding)

--- projects/allinone/src/test/java/org/batfish/minesweeper/smt/SmtReachabilityTest.java
+++ projects/allinone/src/test/java/org/batfish/minesweeper/smt/SmtReachabilityTest.java
@@ -80,32 +80,11 @@ public class SmtReachabilityTest {
         // read the configurations from the filesystem
         Runfiles runfiles = Runfiles.create();
 
-        // String configPath = runfiles.rlocation("batfish/networks/repair-examples/ring/ring10/unbroken");
-        // String configPath = runfiles.rlocation("batfish/networks/repair-examples/ring/ring10/acl_remove_half");
-        // String configPath = runfiles.rlocation("batfish/networks/repair-examples/ring/ring10/rm-neighbor_one");
-        // String configPath = runfiles.rlocation("batfish/networks/repair-examples/ring/ring10/rm-network_half");
-
-        String configPath = runfiles.rlocation("batfish/networks/community_networks/0001");
-
-        // -------------------------------------------------------------
-
-        // String configPath = runfiles.rlocation("batfish/networks/userstudy_network");
-
-        // String configPath = runfiles.rlocation("batfish/networks/userstudy_network_ospf");
-        // String configPath = runfiles.rlocation("batfish/networks/userstudy_network_new");
         // String configPath = runfiles.rlocation("batfish/networks/userstudy_network");
         // String configPath = runfiles.rlocation("batfish/networks/userstudy_network_hard");
         // String configPath = runfiles.rlocation("batfish/networks/userstudy_network_accessment");
         // String configPath = runfiles.rlocation("batfish/networks/userstudy_network_accessment_intro");
         // String configPath = runfiles.rlocation("batfish/networks/userstudy_network_coursera");
-        // String configPath = runfiles.rlocation("batfish/networks/symbolic_configuration_network");
-
-        // -------------------------------------------------------------
-
-        // String configPath = runfiles.rlocation("batfish/networks/testing_networks/sub-prefixes_0001");
-        // String configPath = runfiles.rlocation("batfish/networks/testing_networks/sub-prefixes_0002");
-        // String configPath = runfiles.rlocation("batfish/networks/testing_networks/deny_permit");
-        // String configPath = runfiles.rlocation("batfish/networks/testing_networks/deny_community");
 
         // -------------------------------------------------------------
 
@@ -123,23 +102,16 @@ public class SmtReachabilityTest {
         // String configPath = runfiles.rlocation("batfish/benchmarks/Lines/line10");
         // String configPath = runfiles.rlocation("batfish/benchmarks/Lines/line100");
         // String configPath = runfiles.rlocation("batfish/benchmarks/Lines/line1000");
+        // String configPath = runfiles.rlocation("batfish/benchmarks/Lines/line2000");
         // String configPath = runfiles.rlocation("batfish/benchmarks/Lines/line5000");
         // String configPath = runfiles.rlocation("batfish/benchmarks/Lines/line10000");
 
         // -------------------------------------------------------------
 
-        // String configPath = runfiles.rlocation("batfish/benchmarks/Lines-lite/line10");
-        // String configPath = runfiles.rlocation("batfish/benchmarks/Lines-lite/line100");
-        // String configPath = runfiles.rlocation("batfish/benchmarks/Lines-lite/line1000");
-        // String configPath = runfiles.rlocation("batfish/benchmarks/Lines-lite/line5000");
-        // String configPath = runfiles.rlocation("batfish/benchmarks/Lines-lite/line10000");
-
-        // -------------------------------------------------------------
-
-        // String configPath = runfiles.rlocation("batfish/benchmarks/Internet2/");
         // String configPath = runfiles.rlocation("batfish/benchmarks/Bics/bgp");
         // String configPath = runfiles.rlocation("batfish/benchmarks/Columbus/bgp");
         // String configPath = runfiles.rlocation("batfish/benchmarks/USCarrier/bgp");
+        // String configPath = runfiles.rlocation("batfish/benchmarks/Internet2/");
 
         TestrigText _testrig = loadConfigurations(configPath);
         _batfish = BatfishTestUtils.getBatfishFromTestrigText(_testrig, _temp);
@@ -163,12 +135,6 @@ public class SmtReachabilityTest {
     @Test
     public void testReachability() {
         final ReachabilityQuestion question = new ReachabilityQuestion();
-
-        // ospf
-        question.setIngressNodeRegex("customer");
-        question.setFinalNodeRegex("isp1");
-        IpWildcard ipWildcard = IpWildcard.parse("198.51.100.0/24");
-        question.setDstIps(Set.of(ipWildcard));
 
         // Specification 1: Customer reachability
         // question.setIngressNodeRegex("customer");
@@ -255,22 +221,6 @@ public class SmtReachabilityTest {
 
         // -------------------------------------------------------------
 
-        // sub-prefixes
-        // question.setIngressNodeRegex("r2");
-        // question.setFinalNodeRegex("isp1");
-        // IpWildcard ipWildcard = IpWildcard.parse("10.0.0.0/8");
-        // question.setDstIps(Set.of(ipWildcard));
-
-        // -------------------------------------------------------------
-
-        // deny-permit and permit-permit
-        // question.setIngressNodeRegex("customer");
-        // question.setFinalNodeRegex("isp1");
-        // IpWildcard ipWildcard = IpWildcard.parse("192.168.0.0/16");
-        // question.setDstIps(Set.of(ipWildcard));
-
-        // -------------------------------------------------------------
-
         // FatTree benchmarks - fattree4pol
         // question.setIngressNodeRegex(".*");
         // question.setFinalNodeRegex("edge-19");
@@ -322,60 +272,36 @@ public class SmtReachabilityTest {
         // -------------------------------------------------------------
 
         // Line benchmarks - line10
-        // question.setIngressNodeRegex("customer");
-        // question.setFinalNodeRegex("isp1");
-        // IpWildcard ipWildcard = IpWildcard.parse("10.2.2.0/24");
-        // question.setDstIps(Set.of(ipWildcard));
-        // question.setNegate(true);
-        //
         // question.setIngressNodeRegex(".*");
         // question.setFinalNodeRegex("isp1");
         // IpWildcard ipWildcard = IpWildcard.parse("10.1.1.0/24");
         // question.setDstIps(Set.of(ipWildcard));
 
         // Line benchmarks - line100
-        // question.setIngressNodeRegex("customer");
-        // question.setFinalNodeRegex("isp1");
-        // IpWildcard ipWildcard = IpWildcard.parse("10.2.2.0/24");
-        // question.setDstIps(Set.of(ipWildcard));
-        // question.setNegate(true);
-        //
         // question.setIngressNodeRegex(".*");
         // question.setFinalNodeRegex("isp1");
         // IpWildcard ipWildcard = IpWildcard.parse("10.5.4.0/24");
         // question.setDstIps(Set.of(ipWildcard));
 
         // Line benchmarks - line1000
-        // question.setIngressNodeRegex("customer");
-        // question.setFinalNodeRegex("isp1");
-        // IpWildcard ipWildcard = IpWildcard.parse("10.2.2.0/24");
-        // question.setDstIps(Set.of(ipWildcard));
-        // question.setNegate(true);
-        //
         // question.setIngressNodeRegex(".*");
         // question.setFinalNodeRegex("isp1");
         // IpWildcard ipWildcard = IpWildcard.parse("10.5.49.0/24");
         // question.setDstIps(Set.of(ipWildcard));
 
-        // Line benchmarks - line5000
-        // question.setIngressNodeRegex("customer");
+        // Line benchmarks - line2000
+        // question.setIngressNodeRegex(".*");
         // question.setFinalNodeRegex("isp1");
-        // IpWildcard ipWildcard = IpWildcard.parse("10.2.2.0/24");
+        // IpWildcard ipWildcard = IpWildcard.parse("10.10.49.0/24");
         // question.setDstIps(Set.of(ipWildcard));
-        // question.setNegate(true);
-        //
+
+        // Line benchmarks - line5000
         // question.setIngressNodeRegex(".*");
         // question.setFinalNodeRegex("isp1");
         // IpWildcard ipWildcard = IpWildcard.parse("10.20.62.0/24");
         // question.setDstIps(Set.of(ipWildcard));
 
         // Line benchmarks - line10000
-        // question.setIngressNodeRegex("customer");
-        // question.setFinalNodeRegex("isp1");
-        // IpWildcard ipWildcard = IpWildcard.parse("10.2.2.0/24");
-        // question.setDstIps(Set.of(ipWildcard));
-        // question.setNegate(true);
-        //
         // question.setIngressNodeRegex(".*");
         // question.setFinalNodeRegex("isp1");
         // IpWildcard ipWildcard = IpWildcard.parse("10.20.124.0/24");
@@ -390,34 +316,207 @@ public class SmtReachabilityTest {
         // question.setDstIps(Set.of(ipWildcard));
         //
         // question.setIngressNodeRegex(".*");
-        // question.setFinalNodeRegex("budapest");
-        // IpWildcard ipWildcard = IpWildcard.parse("200.1.38.0/24");
+        // question.setFinalNodeRegex("peerbrussels_4");
+        // IpWildcard ipWildcard = IpWildcard.parse("128.0.4.0/24");
         // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peerfrankfurt_7");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.1.27.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("luxembourg");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.1.51.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("madrid");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.1.56.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("strasbourg");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.1.64.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("zagreb");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.1.59.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("warsaw");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.1.67.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peergeneva_9");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.1.54.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peermarseille_13");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.1.19.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+
+        // -------------------------------------------------------------
 
         // Columbus benchmarks
         // question.setIngressNodeRegex(".*");
         // question.setFinalNodeRegex("grenada");
         // IpWildcard ipWildcard = IpWildcard.parse("200.2.55.0/24");
         // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peerdominicanttrepublic_1");
+        // IpWildcard ipWildcard = IpWildcard.parse("128.0.1.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peernodeid1_2");
+        // IpWildcard ipWildcard = IpWildcard.parse("128.0.4.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peernodeid31_6");
+        // IpWildcard ipWildcard = IpWildcard.parse("128.0.6.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("nodeid44");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.2.100.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("nodeid50");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.2.101.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peerstttcroix_13");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.2.102.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peernodeid46_10");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.2.22.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peerpanama_12");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.2.35.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("curacao");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.2.37.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+
+        // -------------------------------------------------------------
 
         // USCarrier benchmarks
         // question.setIngressNodeRegex(".*");
         // question.setFinalNodeRegex("peerjacksonvilleid5_8");
         // IpWildcard ipWildcard = IpWildcard.parse("128.0.8.0/24");
         // question.setDstIps(Set.of(ipWildcard));
-
-        // Internet2 benchmarks
-        // question.setIngressNodeRegex("hous-re1");
-        // question.setFinalNodeRegex("hous-re1");
-        // IpWildcard ipWildcard = IpWildcard.parse("171.67.234.0/24");
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("peeralbany_0");
+        // IpWildcard ipWildcard = IpWildcard.parse("128.0.0.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("cartersville");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.4.127.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("kinston");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.4.145.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("warwick");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.4.169.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("waynesboro");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.4.172.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("greenwood");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.4.178.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("hagerstown");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.4.197.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("aiken");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.4.202.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex(".*");
+        // question.setFinalNodeRegex("ringgold");
+        // IpWildcard ipWildcard = IpWildcard.parse("200.4.214.0/24");
         // question.setDstIps(Set.of(ipWildcard));
 
         // -------------------------------------------------------------
 
-        // AED repair-examples ring10
-        // question.setIngressNodeRegex("r0");
-        // question.setFinalNodeRegex("r2");
-        // IpWildcard ipWildcard = IpWildcard.parse("70.0.2.0/24");
+        // Internet2 benchmarks
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("100.64.10.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("205.213.200.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("128.10.50.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("128.2.100.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("128.206.50.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("128.101.10.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("63.193.200.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("198.48.92.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("74.115.8.0/24");
+        // question.setDstIps(Set.of(ipWildcard));
+        //
+        // question.setIngressNodeRegex("chic-re1");
+        // question.setFinalNodeRegex("wiscr");
+        // IpWildcard ipWildcard = IpWildcard.parse("63.164.11.0/24");
         // question.setDstIps(Set.of(ipWildcard));
 
         final AnswerElement answer = Answerer.create(question, _batfish).answer(_batfish.getSnapshot());
